c5b58ac6f6a074d59bbb1068ca29d70d28f10a11
AvdeevaAnnaN <skazplast@yandex.ru>
2021-11-30 20:21:43 +0700

    first commit

---
+++ app/src/main/java/com/avd/example/mysecondproject/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.avd.example.mysecondproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Буря мглою небо кроет")
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Вихри снежные крутя;")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "То, как зверь, она завоет")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "То заплачет, как дитя,")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "То по кровле обветшалой")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Вдруг соломой зашумит,")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Вдруг соломой зашумит,")
+    }
+
+}
